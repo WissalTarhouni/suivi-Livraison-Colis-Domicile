3f6ac762cade2d4d2e1a8a4bfc31a532e4dc0cba
tarhouniwissal <wissaltarhouomni.wissal@gmail.com>
2021-04-06 01:35:59 +0200

    project  init

---
+++ back-end/src/main/java/com/spring/master/MasterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spring.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSystemApplication.class, args);
+    }
+
+}
---
+++ back-end/src/test/java/com/spring/master/MasterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
